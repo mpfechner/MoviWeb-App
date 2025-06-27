32c6454c90c3b8c14c3594e748f509b98dba8540
michaelfechner <resetknopf@googlemail.com>
2025-06-27 15:18:29 +0200

    Added / Route to app.py, created tables using models.py

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
+import os
 from flask import Flask
+from models import db
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'movies.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return "Welcome to MoviWeb App!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
